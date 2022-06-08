e9204ef9747611de90ea3d8b9641b0de34670c95
Aleksey-Smouzh <xmz@tut.by>
2022-06-08 22:21:57 +0200

    react button src

--- like_button.js
+++ like_button.js
@@ -1,24 +1,24 @@
-"use strict";
+// "use strict";
 
-const e = React.createElement;
+// const e = React.createElement;
 
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
+// class LikeButton extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { liked: false };
+//   }
 
-  render() {
-    if (this.state.liked) {
-      return "You liked this.";
-    }
+//   render() {
+//     if (this.state.liked) {
+//       return "You liked this.";
+//     }
 
-    return e(
-      "button",
-      { onClick: () => this.setState({ liked: true }) },
-      "Like"
-    );
-  }
-}
-const domContainer = document.querySelector("#like_button_container");
-ReactDOM.render(e(LikeButton), domContainer);
+//     return e(
+//       "button",
+//       { onClick: () => this.setState({ liked: true }) },
+//       "Like"
+//     );
+//   }
+// }
+// const domContainer = document.querySelector("#like_button_container");
+// ReactDOM.render(e(LikeButton), domContainer);
---
+++ src/like_button.js
@@ -0,0 +1,22 @@
+import bad from '../img/'
+
+
+"use strict";
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return "You liked this.";
+    }
+
+    return <button onClick={() => this.setState({ liked: true })}> <img src="" alt="" /> </button>;
+  }
+}
+
+let domContainer = document.querySelector("#like_button_container");
+ReactDOM.render(<LikeButton />, domContainer);
